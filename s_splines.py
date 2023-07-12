9d0617b5ee000c44411ab77abc23b13811558a91
Jasper <jasper@avalor.ai>
2023-07-12 16:41:40 +0200

    first two segements

--- s_splines.py
+++ s_splines.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from typing import List, Tuple, Union, Optional, Dict, Set, Any, Type
-import numpy as np
 
 class ProfileParams:
     """
@@ -56,27 +55,112 @@ class Profile:
     def __init__(self, profile_params : ProfileParams):
         time : Time = Time.from_profile_params(profile_params) 
         self.k_1 : np.ndarray =  self.profile_time_segement_0(time, profile_params)
+        self.k_2 : np.ndarray = self.profile_time_segement_1(time, profile_params)
 
-
+    ########### Segment 0
     def profile_time_segement_0(self, time : Time, profile_params : ProfileParams) -> np.ndarray:
         """
         Calculates the velocities within segment one
         """
-        segment_time : float = time.t_1
-        j_max : float = profile_params.j_max
-        delta_t : float = profile_params.delta_t
         profile : np.ndarray = np.array([])
-
-        for t in range(int(segment_time/delta_t)):
-            current_time : float = i / delta_t
-
-            jerk = np.sin( (np.pi*(t - 0.0)) /(2.0*segment_time) )
-
-
+        for t in range(int(time.t_1/profile_params.delta_t)):
+            current_time : float = t * profile_params.delta_t
+            vel = self._segment_0_vel(time, profile_params, current_time)
+            profile = np.append(profile, vel)
         return profile
+    
+    def _segment_0_jerk(self, time : Time,profile_params : ProfileParams, current_time : float) -> float:
+        """
+        Calculates the jerk of the zeroth segment
+        """
+        passed_time = 0.0
+        return profile_params.j_max * np.sin((np.pi*(current_time - passed_time))/(2.0 * time.t_1 ))
+    
+    def _segment_0_acc(self, time : Time, profile_params : ProfileParams, current_time : float, ) -> float:
+        """
+        Calculates the acceleration of the zeroth segment
+        """
+        constant_a : float = 0.63662 * time.t_1
+        return -0.63662 * time.t_1 * profile_params.j_max * np.cos((1.5708 * current_time) / time.t_1) + constant_a
+    
+    def _segment_0_vel(self, time : Time,profile_params : ProfileParams, current_time : float) -> float:
+        return time.t_1 * (0.63662 * current_time - 0.405284 * time.t_1 * profile_params.j_max *  np.sin((1.5708 * current_time) / time.t_1)) + profile_params.v_init
 
-    def get_velocity_at_time(current_time : float) -> float:
+
+    ########### Segment 1
+    def profile_time_segement_1(self, time : Time, profile_params : ProfileParams) -> np.ndarray:
+        """
+        Calculates the velocities within segment one
+        """
+        profile : np.ndarray = np.array([])
+        for t in range(int(time.t_1/profile_params.delta_t)):
+            current_time : float = t * profile_params.delta_t
+            vel = self._segment_1_vel(time, profile_params, current_time)
+            profile = np.append(profile, vel)
+        return profile
+    
+    def _segment_1_jerk(self, time : Time,profile_params : ProfileParams, current_time : float) -> float:
+        """
+        Calculates the jerk of the zeroth segment
+        """
+        return profile_params.j_max 
+    
+    def _segment_1_acc(self, time : Time, profile_params : ProfileParams, current_time : float, ) -> float:
+        """
+        Calculates the acceleration of the zeroth segment
+        """
+        constant_a : float = self._segment_0_acc(time, profile_params, time.t_1)
+        return profile_params.j_max * current_time + constant_a
+    
+    def _segment_1_vel(self, time : Time,profile_params : ProfileParams, current_time : float) -> float:
+        constant_a : float = self._segment_0_acc(time, profile_params, time.t_1)
+        constant_v : float = self._segment_0_vel(time,profile_params, time.t_1) 
+        return  0.5* profile_params.j_max * current_time**2 + constant_a*current_time + constant_v
+
+    ########### Segment 1
+    def profile_time_segement_1(self, time : Time, profile_params : ProfileParams) -> np.ndarray:
+        """
+        Calculates the velocities within segment one
+        """
+        profile : np.ndarray = np.array([])
+        for t in range(int(time.t_1/profile_params.delta_t)):
+            current_time : float = t * profile_params.delta_t
+            vel = self._segment_1_vel(time, profile_params, current_time)
+            profile = np.append(profile, vel)
+        return profile
+    
+    def _segment_1_jerk(self, time : Time,profile_params : ProfileParams, current_time : float) -> float:
+        """
+        Calculates the jerk of the zeroth segment
+        """
+        return profile_params.j_max 
+    
+    def _segment_1_acc(self, time : Time, profile_params : ProfileParams, current_time : float, ) -> float:
+        """
+        Calculates the acceleration of the zeroth segment
+        """
+        constant_a : float = self._segment_0_acc(time, profile_params, time.t_1)
+        return profile_params.j_max * current_time + constant_a
+    
+    def _segment_1_vel(self, time : Time,profile_params : ProfileParams, current_time : float) -> float:
+        constant_a : float = self._segment_0_acc(time, profile_params, time.t_1)
+        constant_v : float = self._segment_0_vel(time,profile_params, time.t_1) 
+        return  0.5* profile_params.j_max * current_time**2 + constant_a*current_time + constant_v
+    def get_velocity_at_time(self,current_time : float) -> float:
         """
         Retrieves the velocity at a specified time
         """
         return current_time
+    
+
+def main():
+    profile_params : ProfileParams = ProfileParams(100.0,1.0,1.0,1,0.2,0.01)
+    profile : Profile = Profile(profile_params)
+
+    total_prof : np.ndarray = np.concatenate([profile.k_1,profile.k_2])
+
+    plt.plot(range(len(total_prof)), total_prof)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
--- test_s_splines.py
+++ test_s_splines.py
@@ -1,4 +1,17 @@
 import s_splines
 
-def test_profile():
-    assert 0 == 0 
+def test_profile_initial_vel():
+    """
+    Testing the profile if no distance is provided
+    """
+    profile_params : s_splines.ProfileParams = s_splines.ProfileParams(100.0,2.0,1.0,1,0.2,0.01)
+    profile : s_splines.Profile = s_splines.Profile(profile_params)
+    assert profile.k_1[0] == 2.0, "The initial velocity is not 2.0 but: {profile.k_1[0]}"
+
+    profile_params : s_splines.ProfileParams = s_splines.ProfileParams(100.0,1.0,1.0,1,0.2,0.01)
+    profile : s_splines.Profile = s_splines.Profile(profile_params)
+    assert profile.k_1[0] == 1.0, "The initial velocity is not 1.0 but: {profile.k_1[0]}"
+
+    profile_params : s_splines.ProfileParams = s_splines.ProfileParams(100.0,0.0,1.0,1,0.2,0.01)
+    profile : s_splines.Profile = s_splines.Profile(profile_params)
+    assert profile.k_1[0] == 0.0, "The initial velocity is not 0.0 but: {profile.k_1[0]}"
